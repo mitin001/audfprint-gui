e9487826b2c26702cc15d11329a5fcfe7b18ebb0
Andrey Mitin <amitin@gomodus.com>
2022-03-03 23:21:52 -0600

    Ability to use relative filenames

--- src/scenes/FingerprintPage/ReviewAudioFiles.jsx
+++ src/scenes/FingerprintPage/ReviewAudioFiles.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from 'react';
 
+const slash = '/';
+
 export default function ReviewAudioFiles() {
   const [filenames, setFilenames] = useState([]);
   const [fileTypes, setFileTypes] = useState('.mp3,.wav,.flac');
-  window.ipc.on('audioDirectoryOpened', (event, { filePaths }) => {
-    setFilenames(filePaths);
-  });
+  const [levels, setLevels] = useState(0);
+
+  const [firstFilename = ''] = filenames;
   const trimmedFileTypes = fileTypes.split(',').map((fileType) => fileType.trim());
   const filteredFilenames = filenames.filter((filename) => (
     trimmedFileTypes.some((fileType) => filename.indexOf(fileType) !== -1)
-  ));
+  )).map((filename) => {
+    const parts = filename.split(slash);
+    const { length } = parts;
+    if (length <= levels) {
+      return filename;
+    }
+    return parts.slice(levels, length).join(slash);
+  });
+  const maxLevels = firstFilename.split(slash).length - 1;
+
+  window.ipc.on('audioDirectoryOpened', (event, { filePaths }) => {
+    setFilenames(filePaths);
+  });
+
   return (
     <div className="ui">
       <div className="ui labeled input">
@@ -21,6 +36,18 @@ export default function ReviewAudioFiles() {
           placeholder=".mp3,.wav,.flac"
           value={fileTypes}
           onChange={({ target: { value } }) => setFileTypes(value)}
+        />
+      </div>
+      <div className="ui labeled input">
+        <div className="ui label">
+          Levels
+        </div>
+        <input
+          type="number"
+          min={0}
+          max={maxLevels}
+          value={levels}
+          onChange={({ target: { value } }) => setLevels(value)}
         />
       </div>
       <ol>
