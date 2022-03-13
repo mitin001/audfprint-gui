53ab51a5245e4297c1064f9292427f186c818a79
Andrey Mitin <amitin@gomodus.com>
2022-03-12 19:53:55 -0600

    Replace Semantic UI with MUI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppLoader from './AppLoader';
-import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<AppLoader />, document.getElementById('root'));
