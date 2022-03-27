04c3c2d79eb591003ea1f0fb121f2603cab9e729
Andrey Mitin <amitin@gomodus.com>
2022-03-26 23:23:21 -0500

    Use a bottom navigation for switching between Fingerprint and Match pages; use a drawer for switching between fingerprint databases

--- src/AppContent.js
+++ src/AppContent.js
@@ -1,183 +1,42 @@
-import React, { useState } from 'react';
-import { styled } from '@mui/material/styles';
+import React, { useRef, useState } from 'react';
 import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Paper from '@mui/material/Paper';
 import { IoMdFingerPrint } from 'react-icons/io';
 import { CgReorder } from 'react-icons/cg';
-import ReactTooltip from 'react-tooltip';
-import mainTheme from './theme';
 import FingerprintPage from './scenes/FingerprintPage/FingerprintPage';
 import MatchPage from './scenes/MatchPage/MatchPage';
 
-const drawerWidth = 240;
-
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
-  }),
-);
-
-export default function AppContent() {
-  const theme = mainTheme;
-  const [open, setOpen] = useState(false);
-  const [page, setPage] = useState(<FingerprintPage />);
-
+export default function FixedBottomNavigation() {
+  const ref = useRef(null);
+  const [index, setIndex] = useState(0);
+  const pages = [
+    <FingerprintPage />,
+    <MatchPage />,
+  ];
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ pb: 7 }} ref={ref}>
       <CssBaseline />
-      <AppBar position="fixed" open={open} theme={theme}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={() => setOpen(true)}
-            edge="start"
-            sx={{
-              marginRight: 5,
-              ...(open && { display: 'none' }),
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Audio Fingerprinter
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={() => setOpen(false)}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          <ListItemButton
-            onClick={() => setPage(<FingerprintPage />)}
-            sx={{
-              minHeight: 48,
-              justifyContent: open ? 'initial' : 'center',
-              px: 2.5,
-            }}
-          >
-            <ListItemIcon
-              sx={{
-                minWidth: 0,
-                mr: open ? 3 : 'auto',
-                justifyContent: 'center',
-              }}
-            >
-              <IoMdFingerPrint
-                data-delay-show="500"
-                data-tip="Fingerprint"
-                size={25}
-              />
-              <ReactTooltip />
-            </ListItemIcon>
-            <ListItemText primary="Fingerprint" sx={{ opacity: open ? 1 : 0 }} />
-          </ListItemButton>
-          <ListItemButton
-            onClick={() => setPage(<MatchPage />)}
-            sx={{
-              minHeight: 48,
-              justifyContent: open ? 'initial' : 'center',
-              px: 2.5,
-            }}
-          >
-            <ListItemIcon
-              sx={{
-                minWidth: 0,
-                mr: open ? 3 : 'auto',
-                justifyContent: 'center',
-              }}
-            >
-              <CgReorder
-                data-delay-show="500"
-                data-tip="Match"
-                size={25}
-              />
-              <ReactTooltip />
-            </ListItemIcon>
-            <ListItemText primary="Match" sx={{ opacity: open ? 1 : 0 }} />
-          </ListItemButton>
-        </List>
-      </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
-        {page}
-      </Box>
+      {pages[index]}
+      <Paper
+        sx={{
+          position: 'fixed', bottom: 0, left: 0, right: 0,
+        }}
+        elevation={3}
+      >
+        <BottomNavigation
+          showLabels
+          value={index}
+          onChange={(event, newIndex) => {
+            setIndex(newIndex);
+          }}
+        >
+          <BottomNavigationAction label="Fingerprint" icon={<IoMdFingerPrint size={25} />} />
+          <BottomNavigationAction label="Match" icon={<CgReorder size={25} />} />
+        </BottomNavigation>
+      </Paper>
     </Box>
   );
 }
--- src/scenes/FingerprintPage/FingerprintPage.jsx
+++ src/scenes/FingerprintPage/FingerprintPage.jsx
@@ -1,22 +1,167 @@
-import React from 'react';
-import { FaFileAudio } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
 import { Box, Button } from '@mui/material';
-import theme from '../../theme';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { IoMdAddCircle } from 'react-icons/io';
+import { FaFileAudio } from 'react-icons/fa';
+import ReactTooltip from 'react-tooltip';
+import mainTheme from '../../theme';
 import ReviewAudioFiles from './ReviewAudioFiles';
 
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
 export default function FingerprintPage() {
+  const theme = mainTheme;
+  const [open, setOpen] = useState(false);
+
   return (
-    <Box>
-      <Button
-        theme={theme}
-        variant="contained"
-        startIcon={<FaFileAudio size={25} />}
-        onKeyPress={() => window.ipc.send('openAudioDirectory')}
-        onClick={() => window.ipc.send('openAudioDirectory')}
-      >
-        Select directory with audio files
-      </Button>
-      <ReviewAudioFiles />
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open} theme={theme}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() => setOpen(true)}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            New Fingerprint Database
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={() => setOpen(false)}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          <ListItemButton
+            sx={{
+              minHeight: 48,
+              justifyContent: open ? 'initial' : 'center',
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
+              sx={{
+                minWidth: 0,
+                mr: open ? 3 : 'auto',
+                justifyContent: 'center',
+              }}
+            >
+              <IoMdAddCircle
+                data-delay-show="500"
+                data-tip="New"
+                size={25}
+              />
+              <ReactTooltip />
+            </ListItemIcon>
+            <ListItemText primary="New" sx={{ opacity: open ? 1 : 0 }} />
+          </ListItemButton>
+        </List>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        <Box>
+          <Button
+            theme={theme}
+            variant="contained"
+            startIcon={<FaFileAudio size={25} />}
+            onKeyPress={() => window.ipc.send('openAudioDirectory')}
+            onClick={() => window.ipc.send('openAudioDirectory')}
+          >
+            Select directory with audio files
+          </Button>
+          <ReviewAudioFiles />
+        </Box>
+      </Box>
     </Box>
   );
 }
