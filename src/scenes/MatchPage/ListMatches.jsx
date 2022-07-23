595537870f4ef79e79f9247aec4822e859d33383
Andrey Mitin <amitin@gomodus.com>
2022-07-23 18:52:49 -0500

    Ability to search matches

--- src/scenes/MatchPage/ListMatches.jsx
+++ src/scenes/MatchPage/ListMatches.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import {
-  Box, Button, FormControl, IconButton, InputLabel, MenuItem, Select,
+  Box, Button, FormControl, IconButton, InputLabel,
+  MenuItem, Select, OutlinedInput, InputAdornment,
 } from '@mui/material';
 import { CgExport } from 'react-icons/cg';
+import { HiOutlineSearch } from 'react-icons/hi';
 import { ImSortAmountAsc, ImSortAmountDesc } from 'react-icons/all';
 import MatchCard from './MatchCard';
 import theme from '../../theme';
@@ -10,6 +12,7 @@ import theme from '../../theme';
 export default function ListMatches(props) {
   const { filename, matchData } = props || {};
   const { error, parsedMatches = [] } = matchData || {};
+  const [search, setSearch] = useState('');
   const [orderBy, setOrderBy] = useState();
   const [desc, setDesc] = useState(false);
   const compare = (a, b) => {
@@ -25,6 +28,7 @@ export default function ListMatches(props) {
     }
     return 1;
   };
+  const filter = (a) => JSON.stringify(Object.values(a)).toUpperCase().indexOf(search.toUpperCase()) !== -1;
 
   return (
     <Box>
@@ -75,9 +79,35 @@ export default function ListMatches(props) {
       >
         {desc ? <ImSortAmountDesc /> : <ImSortAmountAsc />}
       </IconButton>
+      <FormControl
+        sx={{
+          mb: 2, ml: 2, minWidth: 200, float: 'right',
+        }}
+        size="small"
+        variant="outlined"
+      >
+        <InputLabel htmlFor="search-field">Search</InputLabel>
+        <OutlinedInput
+          id="search-field"
+          type="text"
+          value={search}
+          onChange={({ target: { value } }) => setSearch(value)}
+          endAdornment={(
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="search"
+                edge="end"
+              >
+                <HiOutlineSearch />
+              </IconButton>
+            </InputAdornment>
+          )}
+          label="Password"
+        />
+      </FormControl>
       <pre>{error || ''}</pre>
       {
-        parsedMatches.sort(compare).map((match) => (
+        parsedMatches.filter(filter).sort(compare).map((match) => (
           <MatchCard
             key={match.name + match.database}
             name={match.name}
